2bed3150b40086cc154d57c4f996b9b0e5659a2c
Abhishek Sriram <68271853+Noobsiecoder@users.noreply.github.com>
2021-01-29 14:26:52 +0530

    added .gitignore

--- types/build/reverse.js
+++
@@ -1,9 +0,0 @@
-/* Program to reverse a number or a string */
-var reverseNum = function (value) {
-  return parseInt(value.toString().split("").reverse().join(""));
-};
-var reverseString = function (value) {
-  return value.split("").reverse().join("");
-};
-console.log(reverseString("Hello from TypeScript!")); // !tpircSepyT morf olleH
-console.log(reverseNum(123)); // 321
